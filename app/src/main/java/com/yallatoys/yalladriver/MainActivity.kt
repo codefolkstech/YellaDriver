9537b9dbc550d8032051d49dcc07b3e2e506bf11
Arun Rajendran <arun@aspinlabs.com>
2021-12-18 00:48:14 +0530

    bottom navigation bar completed successfully

--- app/src/main/java/com/yallatoys/yalladriver/MainActivity.kt
+++ app/src/main/java/com/yallatoys/yalladriver/MainActivity.kt
@@ -2,10 +2,29 @@ package com.yallatoys.yalladriver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var item: MenuItem
+    private lateinit var deliveryCount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.single_job_row)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        item = menu?.findItem(R.id.deliveryCount)!!
+        val view = item.actionView
+
+        deliveryCount = view.findViewById(R.id.textviewDeliveryCount)
+        deliveryCount.text = "12"
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
